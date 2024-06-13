7c1b775234cf5a2f2d91188a4f04d933187973e8
Avinash Singh <avinashsinghpro11@gmail.com>
2024-06-13 17:12:28 +0200

    removed empty test case and unused annotation

--- src/main/java/com/assignment/bookstore/model/OrderResponse.java
+++ src/main/java/com/assignment/bookstore/model/OrderResponse.java
@@ -1,16 +1,12 @@
 package com.assignment.bookstore.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class OrderResponse {
     private double totalPrice;
     private Map<String, OrderDetail> evaluatedOrder;
